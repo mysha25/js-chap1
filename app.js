a9dcf7a2ee0ba08305bfd99e2c282d8cdfd093c1
mysha25 <142077817+mysha25@users.noreply.github.com>
2023-10-14 22:56:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+//CHAPTER ONE
+// practice exercise:1.3
+alert("Hi there!");
+// practice exercise:1.4
+let a = 10;
+// i set value of a is 10
+console.log(a);//this will print the value of a in browser.....
+/* I'm a multi-line comment. Whatever is between the slash asterisk and
+the asterisk slash will not get executed.
+console.log("I'm not logged, because I'm a comment");
+*/
+//CHAPTER PROJECT
+// console.log(mysha saleem);
+/* I'm a multi-line comment. Whatever is between the slash asterisk and
+the asterisk slash will not get executed.
+console.log("I'm not logged, because I'm a comment");
+*/
